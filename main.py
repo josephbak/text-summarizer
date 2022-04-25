c0b1660a86883a6fdc82bdffb5a9fc2351718a02
Joseph Bak <joseph.bak31415@gmail.com>
2022-04-24 22:16:09 -0400

    main modified

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import nltk
 from nltk.tokenize import sent_tokenize
+import sys
 import process
 import frequency
 
 
 def main():
-    # Get the text/article from the Internet or prompt the user
-    text = "Some text from main file"
 
-    # sentence-tokenized copy of text
-    sentences = sent_tokenize(text)
-    sentences_num = len(sentences)
+    if len(sys.argv) == 2:
+        # When file is given in a way: python main.py filename
+        return
+    else:
+        # In case, it's given as an address, then perform some scrapping.
+
+        # Get the text/article from the Internet or prompt the user
+        text = "Some text from main file"
+
+        # sentence-tokenized copy of text
+        sentences = sent_tokenize(text)
+        sentences_num = len(sentences)
 
 
 
--- tf_idf.py
+++ tf_idf.py
@@ -1,30 +1,6 @@
 import nltk
 import numpy as np
 
-
-def main():
-
-
-
-    return
-
-
-
-text = input("file.")
-
-sentences = nltk.tokenize.sent_tokenize(text) # NLTK function
-total_documents = len(sentences)
-
 def compute_idf():
 
     return
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
